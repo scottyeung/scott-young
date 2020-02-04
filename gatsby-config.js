8527cd40fdda2aa948d6d923689b28180fca1d41
Scott Yeung <micold@gmail.com>
2020-02-04 23:37:44 +0000

    `gatsby-plugin-google-analytics`

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,21 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-157799812-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "parodybiz.co.uk",
+      },
+    },
   ],
 }
