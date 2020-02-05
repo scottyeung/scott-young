7addea07810ed192b7cbada4c69f5d517438278e
Scott Yeung <micold@gmail.com>
2020-02-05 12:02:16 +0000

    Gatsby GA fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-157799812-1",
+        trackingId: process.env.GA,
         head: false,
         anonymize: true,
       },
